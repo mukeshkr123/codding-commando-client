e66e27c10e6f71e5575693b3d84cc5f706822b9a
mukesh kumar <mkmehta2041@gmail.com>
2024-03-25 17:45:25 +0530

    add content-layer-config

---
+++ contentlayer.config.js
@@ -0,0 +1,55 @@
+import { makeSource, defineDocumentType } from "@contentlayer/source-files";
+import readingTime from "reading-time";
+
+const Blog = defineDocumentType(() => ({
+  name: "Blog",
+  filePathPattern: "**/**/*.mdx",
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true,
+    },
+    publishedAt: {
+      type: "date",
+      required: true,
+    },
+    updatedAt: {
+      type: "date",
+      required: true,
+    },
+    description: {
+      type: "string",
+      required: true,
+    },
+    image: { type: "image" },
+    isPublished: {
+      type: "boolean",
+      default: true,
+    },
+    author: {
+      type: "string",
+      required: true,
+    },
+    tags: {
+      type: "list",
+      of: { type: "string" },
+    },
+  },
+  computedFields: {
+    url: {
+      type: "string",
+      resolve: (doc) => `/blogs/${doc._raw.flattenedPath}`,
+    },
+    readingTime: {
+      type: "json",
+      resolve: (doc) => readingTime(doc.body.raw),
+    },
+  },
+}));
+
+export default makeSource({
+  /* options */
+  contentDirPath: "content",
+  documentTypes: [Blog],
+});
--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+const { withContentlayer } = require("next-contentlayer");
+
 const nextConfig = {
   images: {
     domains: ["utfs.io", "coddingbucket.s3.ap-south-1.amazonaws.com"],
   },
 };
 
-module.exports = nextConfig;
+module.exports = withContentlayer({ ...nextConfig });
